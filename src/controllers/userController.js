de957a81233975f89b3ec6db53d58dddbd7c5913
Sophia Balico <sophiabalico.tds2.senai@gmail.com>
2025-05-20 09:22:17 -0300

    feat: create user model

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,4 @@
-import UserModel from "../models/usermModel.js";
+import UserModel from "../models/user.model.js";
 
 class UserController {
   // GET /api/users
---
+++ src/models/user.model.js
@@ -0,0 +1,21 @@
+import prisma from "../../prisma/prisma.js";
+
+class UserModel {
+  // Obter todos os usuários
+  async findAll() {
+    const users = await prisma.user.findMany();
+
+    return users;
+  }
+
+  // Criar um novo usuário
+  async create(data) {
+    const user = await prisma.user.create({
+      data,
+    });
+
+    return user;
+  }
+}
+
+export default new UserModel();
