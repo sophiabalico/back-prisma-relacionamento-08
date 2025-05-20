c583c043816e3cf49c52a1d565e07af1c027d78c
Sophia Balico <sophiabalico.tds2.senai@gmail.com>
2025-05-20 11:03:13 -0300

    feat: create filter user in model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,10 +2,44 @@ import prisma from "../../prisma/prisma.js";
 
 class UserModel {
   // Obter todos os usuários
-  async findAll() {
-    const users = await prisma.user.findMany();
+  async findAll(name, userType, pagina, limite) {
+    if (Number(pagina) < 1) {
+      pagina = 1;
+    }
 
-    return users;
+    if (Number(limite) < 1 || Number(limite) > 100) {
+      limite = 10;
+    }
+    
+    const skip = (Number(pagina) - 1) * Number(limite);
+
+    const where = {}
+
+    if(name) {
+      where.name = name
+    }
+
+    if(userType) {
+      where.userType = {
+        gte: String(userType),
+      }
+    }
+
+  const users = await prisma.user.findMany({
+      skip,
+      take: Number(limite),
+      where,
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    const totalExibidos = ordens.length;
+    const totalGeral = await prisma.order.count({ 
+      where,
+     });
+
+    return { totalExibidos, totalGeral, users};
   }
 
   // Criar um novo usuário
