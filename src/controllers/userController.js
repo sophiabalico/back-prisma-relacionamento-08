7fc79c79023863b02a9fdc1a4943a55a348f360a
Sophia Balico <sophiabalico.tds2.senai@gmail.com>
2025-05-20 09:14:51 -0300

    feat: create database tables and create userController

---
+++ src/controllers/userController.js
@@ -0,0 +1,63 @@
+import UserModel from "../models/usermModel.js";
+
+class UserController {
+  // GET /api/users
+  async getAllUsers(req, res) {
+    try {
+      const users = await UserModel.findAll();
+      res.json(users);
+    } catch (error) {
+      console.error("Erro ao buscar usuários:", error);
+      res.status(500).json({ error: "Erro ao buscar usuários" });
+    }
+  }
+
+
+  // POST /api/users
+  async createUser(req, res) {
+    try {
+      // Validação básica
+      const {
+        email,
+        password,
+        name,
+        userType,
+        profileImage,
+        bio,
+      } = req.body;
+
+      // Verifica se todos os campos do user foram fornecidos
+      if (
+        !email ||
+        !password ||
+        !name ||
+        !userType
+      ) {
+        return res
+          .status(400)
+          .json({ error: "Todos os campos são obrigatórios" });
+      }
+
+      // Criar o novo usuário
+      const newUser = await UserModel.create(
+        email,
+        password,
+        name,
+        userType,
+        profileImage,
+        bio
+      );
+
+      if (!newUser) {
+        return res.status(400).json({ error: "Erro ao criar usuário" });
+      }
+
+      res.status(201).json(newUser);
+    } catch (error) {
+      console.error("Erro ao criar usuário:", error);
+      res.status(500).json({ error: "Erro ao criar usuário" });
+    }
+  }
+
+}
+export default new UserController();
